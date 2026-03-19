504d85e26dbc3e969bef21b6df4e02049c7363ce
DVV_COMPUTER\biker <dvv153m@gmail.com>
2026-03-19 21:48:27 +0300

    add swagger

---
+++ src/LinkSummary.Api/Configuration/AiClientConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace LinkSummary.Api.Configuration
+{
+    public class AiClientConfig
+    {
+        public const string SectionName = "AiClientConfig";
+
+        public string OpenAiApiKey { get; init; }
+    }
+}
--- src/LinkSummary.Api/Program.cs
+++ src/LinkSummary.Api/Program.cs
@@ -1,12 +1,21 @@
+using LinkSummary.Api.AppStart;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
+var startup = new Startup(builder);
+startup.Initialize();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+if (builder.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+else
+{
+    //app.ApplyCors();
+}
 
 app.UseHttpsRedirection();
 
