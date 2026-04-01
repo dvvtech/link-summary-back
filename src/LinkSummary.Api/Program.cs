aa98456ccb86fb60f61e246fcf1d805aeaa0f883
DVV_COMPUTER\biker <dvv153m@gmail.com>
2026-04-01 22:25:18 +0300

    refactoring

--- src/LinkSummary.Api/Program.cs
+++ src/LinkSummary.Api/Program.cs
@@ -5,15 +5,6 @@ using Microsoft.AspNetCore.HttpOverrides;
 
 var builder = WebApplication.CreateBuilder(args);
 
-/*builder.Services.Configure<ForwardedHeadersOptions>(options =>
-{
-    options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
-
-    // The app is behind a reverse proxy, so do not restrict forwarded headers to loopback only.
-    options.KnownIPNetworks.Clear();
-    options.KnownProxies.Clear();
-});*/
-
 var startup = new Startup(builder);
 startup.Initialize();
 
@@ -30,8 +21,6 @@ else
 }
 
 app.UseForwardedHeaders();
-app.UseHttpsRedirection();
-app.UseAuthorization();
 
 app.MapControllers();
 app.MapHealthChecks("/health", new Microsoft.AspNetCore.Diagnostics.HealthChecks.HealthCheckOptions 
